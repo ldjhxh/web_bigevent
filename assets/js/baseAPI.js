976fc95e3832d7f9a44925ae7734c7c16f581383
羽落 <9997293+feather-falling-dust@user.noreply.gitee.com>
2022-05-10 15:16:31 +0800

    完成个人中心的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,7 +3,7 @@
 // 在这个函数中可以拿到我们给 ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
   // 在发起真正的Ajax请求之前， 统一拼接请求的根路径
-  options.url = 'http://www.liulongbin.top:3007' + options.url
+  options.url = 'http://127.0.0.1:3007' + options.url
 
   // 统一为有权限的接口，设置 headers 请求头
   if (options.url.indexOf('/my/') !== -1) {
